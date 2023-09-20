dcba2b3447bdd48a9c710913d7df6c7137074188
carlosismaelad <carlosismaelad@outlook.com>
2023-09-20 14:43:15 -0300

    Usando o tsconfig, alterando suas configurações e compilando para versões mais antigas do JS

---
+++ 02-compilando-com-TypeScript/main.js
@@ -0,0 +1,30 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const setPilot = (newPilot, spaceship) => __awaiter(this, void 0, void 0, function* () {
+    spaceship.pilot = newPilot;
+});
+const accelerate = (targetSpeed, spaceship) => __awaiter(this, void 0, void 0, function* () {
+    spaceship.speed = targetSpeed;
+});
+const sendToMission = (spaceship) => __awaiter(this, void 0, void 0, function* () {
+    spaceship.inMission = true;
+});
+const spaceship = {
+    name: '',
+    pilot: '',
+    speed: 0,
+    inMission: false,
+};
+const pilot = "Han Solo";
+spaceship.name = "Millennium Falcon";
+setPilot(pilot, spaceship);
+accelerate(50, spaceship);
+sendToMission(spaceship);
+console.log(spaceship);
